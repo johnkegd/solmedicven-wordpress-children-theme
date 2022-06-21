c70aaf50215b5d2f2f2518b805fb02f3f4641d08
John Garcia <johnkegd@gmail.com>
2022-06-22 00:29:45 +0200

    testing enque custom css file and overriting header type 1 with some test container to show as platform info

--- functions.php
+++ functions.php
@@ -1,17 +1,26 @@
 <?php
+
 /**
  * Theme functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
-function machic_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style' , get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'machic-child-style',
+function machic_child_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style(
+        'machic-child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( 'machic-style' ),
+        array('machic-style'),
+        wp_get_theme()->get('Version')
+    );
+    wp_enqueue_style(
+        'machic-child-style',
+        get_stylesheet_directory_uri() . '/custom.css',
+        array('machic-style'),
         wp_get_theme()->get('Version')
     );
 }
 
-add_action(  'wp_enqueue_scripts', 'machic_child_enqueue_styles', 99 );
+add_action('wp_enqueue_scripts', 'machic_child_enqueue_styles', 99);

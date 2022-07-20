ac80b57895439f18ed1655a63cac63d69454ddb1
John Garcia <johnkegd@gmail.com>
2022-07-20 23:31:57 +0200

    not duplicates styles, fixed background color change

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@
 
 function machic_child_enqueue_styles()
 {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style(
         'machic-child-style',
         get_stylesheet_directory_uri() . '/style.css',

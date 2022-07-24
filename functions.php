702acb5ac19c4d53c16211919ec811bdccfd6505
John Garcia <johnkegd@gmail.com>
2022-07-24 15:10:24 +0200

    fixing assets naming spaces error

--- functions.php
+++ functions.php
@@ -8,20 +8,20 @@
 
 function machic_child_enqueue_styles()
 {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . 'style.css');
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 function custom_libraries_activations_scripts()
 {
     wp_enqueue_script(
         'custom-libraries-init-scripts',
-        get_stylesheet_directory_uri() . 'js/custom-libraries-init.js',
+        get_stylesheet_directory_uri() . '/js/custom-libraries-init.js',
         array(),
         false,
         true
     );
     wp_enqueue_script(
         'custom-libraries-init-scripts',
-        get_stylesheet_directory_uri() . 'js/custom-machic.js',
+        get_stylesheet_directory_uri() . '/js/custom-machic.js',
         array(),
         false,
         true
